f0de48c5805b81f8a43aaff2f3ff6f15e9330a7d
Fiziyanali <beyzagumus64@gmail.com>
2021-12-29 15:51:45 +0300

    deneme

---
+++ src/main/java/com/vetApplication/beyzaClinic/BeyzaClinicApplication.java
@@ -0,0 +1,13 @@
+package com.vetApplication.beyzaClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyzaClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyzaClinicApplication.class, args);
+	}
+
+}
